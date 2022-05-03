bc535262114177982b2f1fd1e46ef37daf887c25
Trần Ngọc Nhật <anhchangthode22145@gmail.com>
2022-05-03 09:16:17 +0700

    Cập nhật vào thư viện blockchain

---
+++ block.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 Dave Nash <https://github.com/knasher>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <block.h>
+#include <sha256.h>
+
+Block::Block(uint32_t nIndexIn, const std::string &sDataIn) : _nIndex(nIndexIn), _sData(sDataIn)
+{
+    _nNonce = 0;
+    _tTime = time(nullptr);
+
+    sHash = _CalculateHash();
+}
+
+void Block::MineBlock(uint32_t nDifficulty)
+{
+    char cstr[nDifficulty + 1];
+    for (uint32_t i = 0; i < nDifficulty; ++i)
+    {
+        cstr[i] = '0';
+    }
+    cstr[nDifficulty] = '\0';
+
+    string str(cstr);
+
+    do
+    {
+        _nNonce++;
+        sHash = _CalculateHash();
+    }
+    while (sHash.substr(0, nDifficulty) != str);
+
+    cout << "Block mined: " << sHash << endl;
+}
+
+inline string Block::_CalculateHash() const
+{
+    std::stringstream ss;
+    ss << _nIndex << sPrevHash << _tTime << _sData << _nNonce;
+
+    return sha256(ss.str());
+}
---
+++ blockchain.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Dave Nash <https://github.com/knasher>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <blockchain.h>
+
+Blockchain::Blockchain()
+{
+    _vChain.emplace_back(Block(0, "Genesis Block"));
+    _nDifficulty = 6;
+}
+
+void Blockchain::AddBlock(Block bNew)
+{
+    bNew.sPrevHash = _GetLastBlock().sHash;
+    bNew.MineBlock(_nDifficulty);
+    _vChain.push_back(bNew);
+}
+
+Block Blockchain::_GetLastBlock() const
+{
+    return _vChain.back();
+}
